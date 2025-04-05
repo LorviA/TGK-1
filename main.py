b2e778296099d313b6ed77ff706e414469518025
Yaposha <iopi72725@gmail.com>
2025-04-05 15:19:53 +0300

    Direct3

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ Base.metadata.create_all(bind=engine)
 app = FastAPI()
 app.include_router(user_router)
 app.include_router(zno_router)
+app.include_router(directory_route)
 
 @app.get("/")
 def home():
