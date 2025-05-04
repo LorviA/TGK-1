369c439a81047a9d0400f363810b1de0b6aeae74
Yana0102 <128310255+Yana0102@users.noreply.github.com>
2025-05-04 16:25:24 +0000

    Update main.py

--- main.py
+++ main.py
@@ -5,10 +5,20 @@ from Contollers.user_controller import router as user_router
 from Contollers.zno_controller import router as zno_router
 from Contollers.logger_controller import router as loggers_router
 from DataBase.base import Base, engine
+from fastapi.middleware.cors import CORSMiddleware
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins="http://localhost:3000",
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(user_router)
 app.include_router(zno_router)
 app.include_router(directory_router)
