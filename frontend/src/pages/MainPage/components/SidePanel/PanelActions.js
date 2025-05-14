14a9f337ac919257862e00449ba7b9beb5ae02b6
Yana0102 <128310255+Yana0102@users.noreply.github.com>
2025-05-14 20:04:37 +0300

    Update PanelActions.js

--- frontend/src/pages/MainPage/components/SidePanel/PanelActions.js
+++ frontend/src/pages/MainPage/components/SidePanel/PanelActions.js
@@ -59,17 +59,14 @@ const PanelActions = ({ selectedAppId, onForceRefreshTable }) => {
       // Получаем заявку по ID через API
       const selectedZno = await getZnoById(selectedAppId);
 
-      // Проверка прав и статуса заявки
-      const userId = authData.id;
-
       // ограничение редактирования для ОКО
-      if (userRights === 2 && (![0, 1, 2].includes(selectedZno.id_status))) {
+      if (userRights === 2 && (![0, 1, 2, 3].includes(selectedZno.id_status))) {
         alert('Редактирование заявки запрещено для вас.');
         return;
       }
 
       // ограничение редактирования для работника
-      if (userRights === 3 && (selectedZno.id_user !== userId || ![0, 1, 2].includes(selectedZno.id_status))) {
+      if (userRights === 3 && (![0, 1, 2].includes(selectedZno.id_status))) {
         alert('Редактирование заявки запрещено для вас.');
         return;
       }
